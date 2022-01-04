8a613571abcb4511dbe8de82f11bd4bb8c43c549
Eray Bartan <ebartan@gmail.com>
2022-01-04 09:07:47 +0000

    process.env

--- bodrum-dapp/pages/_app.js
+++ bodrum-dapp/pages/_app.js
@@ -4,7 +4,7 @@ import { MoralisProvider } from "react-moralis";
 function MyApp({ Component, pageProps}) {
 
   return (
-    <MoralisProvider appId={"8tBntj4KUT0ATbZXZ7cQ1mBTn6imRqT3DuK3y3Ae"} serverUrl={"https://cbyty9li9um6.usemoralis.com:2053/server"}>
+    <MoralisProvider appId={process.env.appId} serverUrl={process.env.serverUrl}>
     <Component {...pageProps} />
     </MoralisProvider>)
 }
